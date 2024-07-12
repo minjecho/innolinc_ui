ef660e18d9ce09233e7f222062739bafa3528ba7
fortress-innovation <minje.cho@fortinno.com>
2024-07-12 16:57:27 +0900

    Router setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 // import './App.css';
-import { Link, useNavigate } from "react-router-dom"
 
 // components
 import Login from './components/Login';
@@ -9,8 +8,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
-  //const navigate = useNavigate();
-
   return (
     <div className="App">
       <header className="App-header">
